19b197bf65f0d3a446fcca72cf2f7e3b23145698
Plarget <67307148+Plarget@users.noreply.github.com>
2020-12-21 14:00:18 +0400

    -fix bags

--- game/zombie_invasion/scripts/vscripts/items/item_bonus_stat.lua
+++ game/zombie_invasion/scripts/vscripts/items/item_bonus_stat.lua
@@ -17,7 +17,7 @@ function StrengthTomeUsed( event )
     else
         picker:SetModifierStackCount("tome_strenght_modifier", picker, (picker:GetModifierStackCount("tome_strenght_modifier", picker) + statBonus))
     end
-	picker:CalculateStatBonus()
+	picker:CalculateStatBonus(true)
 end
 
 function AgilityTomeUsed( event )   
@@ -34,7 +34,7 @@ function AgilityTomeUsed( event )
     else
         picker:SetModifierStackCount("tome_agility_modifier", picker, (picker:GetModifierStackCount("tome_agility_modifier", picker) + statBonus))
     end
-	picker:CalculateStatBonus()
+	picker:CalculateStatBonus(true)
 end
 
 function IntellectTomeUsed( event )
@@ -51,7 +51,7 @@ function IntellectTomeUsed( event )
     else
         picker:SetModifierStackCount("tome_intelect_modifier", picker, (picker:GetModifierStackCount("tome_intelect_modifier", picker) + statBonus))
     end
-	picker:CalculateStatBonus()
+	picker:CalculateStatBonus(true)
 end
 ---------------------------------------------------------------
 ---------------------------------------------------------------
