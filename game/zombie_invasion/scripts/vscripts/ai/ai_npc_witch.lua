8515e69750464356ad2d5c7b60bdba7f8638aa56
BossofThisGy3 <82719937+BossofThisGy3@users.noreply.github.com>
2021-06-24 02:07:51 +0300

    Upgrades for Homer
    Upgrades for Homer

---
+++ game/zombie_invasion/scripts/vscripts/ai/ai_npc_witch.lua
@@ -0,0 +1,81 @@
+function Spawn( entityKeyValues )    -- вызывается когда юнит появляется
+    if not IsServer() then        -- если сервер не отвечает
+        return
+    end
+    if thisEntity == nil then    -- если данного юнита не существует
+        return
+    end
+    
+    NoTargetAbility = thisEntity:FindAbilityByName( "medusa_spirit_lance" )
+	NoTargetAbility2 = thisEntity:FindAbilityByName( "lich_sinister_gaze_custom" )
+
+    thisEntity:SetContextThink( "NecroLordThink", NecroLordThink, 0.5 )    -- поведение юнита каждую секунду
+end
+
+function NecroLordThink()
+
+    if ( not thisEntity:IsAlive() ) then        --если юнит мертв
+        return -1  
+    end
+   
+    if GameRules:IsGamePaused() == true then    --если игра приостановлена
+        return 1  
+    end
+	
+	local hp = thisEntity:GetHealthPercent()
+	
+    local enemies = FindUnitsInRadius(
+                        thisEntity:GetTeamNumber(),    --команда юнита
+                        thisEntity:GetOrigin(),        --местоположение юнита
+                        nil,    --айди юнита (необязательно)
+                        1000,    --радиус поиска
+                        DOTA_UNIT_TARGET_TEAM_ENEMY,    -- юнитов чьей команды ищем вражеской/дружественной
+                        DOTA_UNIT_TARGET_HERO,    --юнитов какого типа ищем
+                        DOTA_UNIT_TARGET_FLAG_NONE,    --поиск по флагам
+                        FIND_CLOSEST,    --сортировка от ближнего к дальнему или от дальнего к ближнему
+                        false )
+
+				
+	
+
+	if #enemies > 0 then
+        if NoTargetAbility ~= nil and NoTargetAbility:IsFullyCastable()  then
+				NoTargetAbilityCast()
+			return 1
+		end
+	
+	if hp < 100 then
+		if NoTargetAbility2 ~= nil and NoTargetAbility2:IsFullyCastable()  then    --если абилка существует и её можно использовать
+            for _,unit in pairs(enemies) do
+				if unit then
+				NoTargetAbilityCast2(unit)
+        end
+		end
+		end
+		end
+		end
+	return 0.5 
+end
+
+
+
+function NoTargetAbilityCast(unit)
+      ExecuteOrderFromTable({
+            UnitIndex = thisEntity:entindex(),    --индекс кастера
+            OrderType = DOTA_UNIT_ORDER_CAST_NO_TARGET,    -- тип приказа
+            AbilityIndex = NoTargetAbility:entindex(), -- индекс способности
+            Queue = false,
+        })
+    return 1
+end
+
+function NoTargetAbilityCast2(unit)
+      ExecuteOrderFromTable({
+            UnitIndex = thisEntity:entindex(),    --индекс кастера
+            OrderType = DOTA_UNIT_ORDER_CAST_TARGET,  
+			TargetIndex = unit:entindex(),			-- тип приказа
+            AbilityIndex = NoTargetAbility2:entindex(), -- индекс способности
+            Queue = false,
+        })
+    return 1
+end
---
+++ game/zombie_invasion/scripts/vscripts/data/talantdata.lua
@@ -0,0 +1,89 @@
+talantlist = {
+    --startnumber - это число с которого начинается отсчет на фотографиях и тул типах
+    -- startnumber = 1 картинки, тул типы будут 1-14
+    -- startnumber = 15 картинки, тул типы будут 15-28
+    -- startnumber = 29 картинки, тул типы будут 29-42
+    -- startnumber = 43 картинки, тул типы будут 43-56
+    npc_dota_hero_naga_siren = {
+        naga_ability_1 = { -- название скила, для доты
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-1.png", -- url
+            name = "Talant_npc_dota_hero_axe_STR_name_", -- название скила, для локализации
+            description = "Talant_npc_dota_hero_naga_siren_STR_text_", -- описание
+            buff = "Talant_npc_dota_hero_naga_siren_STR_buff_", -- описание баффа
+            place = {"agi 1", "str 1", "int 1", "don 1"} -- где будет этот скил, если он повторяется
+        },
+        naga_ability_2 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-2.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 2", "str 2", "int 2", "don 2"}
+        },
+        naga_ability_3 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-3.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 3", "str 3", "int 3", "don 3"}
+        },
+        naga_ability_4 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-4.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 4", "str 4", "int 4", "don 4"}
+        },
+        naga_ability_5 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-5.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 5", "str 5", "int 5", "don 5"}
+        },
+        naga_ability_6 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-6.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 6", "str 6", "int 6", "don 6"}
+        },
+        naga_ability_7 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-7.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 7", "str 7", "int 7", "don 7"}
+        },
+        naga_ability_8 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-8.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 8", "str 8", "int 8", "don 8"}
+        },
+        naga_ability_9 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-9.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 9", "str 9", "int 9", "don 9"}
+        },
+        naga_ability_10 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-10.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 10", "str 10", "int 10", "don 10"}
+        },
+        naga_ability_11 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-11.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 11", "str 11", "int 11", "don 11"}
+        },
+        naga_ability_12 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-12.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 12", "str 12", "int 12", "don 12"}
+        },
+        naga_ability_13 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-13.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 13", "str 13", "int 13", "don 13"}
+        },
+        naga_ability_14 = {
+            url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-14.png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_",
+            place = {"agi 14", "str 14", "int 14", "don 14"}
+        },
+    },
+}
+
+
+--[[
+        npc_dota_hero_naga_siren = {
+            str = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_" },
+            agi = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/agi/agi-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_AGI_name_", description = "Talant_npc_dota_hero_naga_siren_AGI_text_", buff = "Talant_npc_dota_hero_naga_siren_AGI_buff_" },
+            int = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/int/int-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_INT_name_", description = "Talant_npc_dota_hero_naga_siren_INT_text_", buff = "Talant_npc_dota_hero_naga_siren_INT_buff_", },
+            don = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/don/don-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_DON_name_", description = "Talant_npc_dota_hero_naga_siren_DON_text_", buff = "Talant_npc_dota_hero_naga_siren_DON_buff_" },
+        },
+        npc_dota_hero_axe = {
+            str = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/agi/agi-talant-", form = ".png", name = "Talant_npc_dota_hero_axe_STR_name_", description = "Talant_npc_dota_hero_axe_STR_text_", buff = "Talant_npc_dota_hero_axe_STR_buff_" },
+            agi = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-", form = ".png", name = "Talant_npc_dota_hero_axe_AGI_name_", description = "Talant_npc_dota_hero_axe_AGI_text_", buff = "Talant_npc_dota_hero_axe_AGI_buff_" },
+            int = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/don/don-talant-", form = ".png", name = "Talant_npc_dota_hero_axe_INT_name_", description = "Talant_npc_dota_hero_axe_INT_text_", buff = "Talant_npc_dota_hero_axe_INT_buff_", },
+            don = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/int/int-talant-", form = ".png", name = "Talant_npc_dota_hero_axe_DON_name_", description = "Talant_npc_dota_hero_axe_DON_text_", buff = "Talant_npc_dota_hero_axe_DON_buff_" },
+        },
+        
+        str = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/str/str-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_STR_name_", description = "Talant_npc_dota_hero_naga_siren_STR_text_", buff = "Talant_npc_dota_hero_naga_siren_STR_buff_" },
+        agi = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/agi/agi-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_AGI_name_", description = "Talant_npc_dota_hero_naga_siren_AGI_text_", buff = "Talant_npc_dota_hero_naga_siren_AGI_buff_" },
+        int = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/int/int-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_INT_name_", description = "Talant_npc_dota_hero_naga_siren_INT_text_", buff = "Talant_npc_dota_hero_naga_siren_INT_buff_", },
+        don = { startnumber = 1, url = "/images/custom_game/talants/img/npc_dota_hero_naga_siren/don/don-talant-", form = ".png", name = "Talant_npc_dota_hero_naga_siren_DON_name_", description = "Talant_npc_dota_hero_naga_siren_DON_text_", buff = "Talant_npc_dota_hero_naga_siren_DON_buff_" },
+        ]]
