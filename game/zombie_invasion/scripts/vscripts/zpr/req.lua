c239cef616d43971cfd80ce77e5834312c632007
Roman Avondale <yandproma@yandex.com>
2021-06-24 00:40:25 +0300

    альфа отображение квестов и ещё чето там

--- game/zombie_invasion/scripts/vscripts/zpr/req.lua
+++ game/zombie_invasion/scripts/vscripts/zpr/req.lua
@@ -1,12 +1,14 @@
 --
 require "zpr/events"
 require "zpr/spawns"
+require "zpr/filters"
 --
 
 --
 function zprInit()
 	print("ZPR LOADING")
 	ZEvent:init()
+	ZFilter:init()
 	print("ZPR LOADED")
 end
 --
