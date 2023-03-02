50e6121b75adb578df581b810a5109985e2ebaba
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2023-03-02 22:01:21 +0300

    S NEW BOSS + FIX

--- game/zombie_invasion/scripts/vscripts/item_drop.lua
+++ game/zombie_invasion/scripts/vscripts/item_drop.lua
@@ -18,6 +18,7 @@ ItemDrop.item_drop = {
 		{items = {"item_bone"}, chance = 100, duration = 20, units = {"npc_classic_skeleton_king"}},  
 		{items = {"item_ess_pudge"}, chance = 13, limit = 4, units = {"npc_classic_warlock"}}, 
 		{items = {"item_necr_heart"}, chance = 20, units = {"npc_classic_warlock"}}, 
+		{items = {"item_basher"}, chance = 100, units = {"npc_boss_slardar"}},
  			
 		{items = {"item_aghanims_shard_roshan"}, units ={"npc_witch_boss_2"}},  
 
