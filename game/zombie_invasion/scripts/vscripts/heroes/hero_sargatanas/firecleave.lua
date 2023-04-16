1097d1535beb33b952d9a8e57c3b828cc437be9b
Plarget <67307148+Plarget@users.noreply.github.com>
2023-04-16 14:55:16 +0400

    asdasd
    ads

--- game/zombie_invasion/scripts/vscripts/heroes/hero_sargatanas/firecleave.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_sargatanas/firecleave.lua
@@ -1,4 +1,6 @@
 LinkLuaModifier( "modifier_ability_firecleave", "heroes/hero_sargatanas/firecleave" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_ability_firecleave_fire", "heroes/hero_sargatanas/firecleave" ,LUA_MODIFIER_MOTION_NONE )
+
 
 if ability_firecleave == nil then
     ability_firecleave = class({})
@@ -45,8 +47,38 @@ function modifier_ability_firecleave:OnAttackLanded(k)
     local caster = self:GetParent()
     local target = k.target
     local attacker = k.attacker
+    local duration = self:GetAbility():GetSpecialValueFor("duration")
     if caster == attacker and not caster:PassivesDisabled() then
+        target:AddNewModifier(caster,self:GetAbility(),"modifier_ability_firecleave_fire",{duration = duration})
         local fx = "particles/econ/items/sven/sven_ti7_sword/sven_ti7_sword_spell_great_cleave_gods_strength_crit_b.vpcf"
         DoCleaveAttack(caster, target, self:GetAbility(), self.great_cleave_damage, self.cleave_starting_width, self.cleave_ending_width, self.cleave_distance, fx)
     end
+end
+
+
+modifier_ability_firecleave_fire = class({
+    IsHidden                = function(self) return false end,
+    IsPurgable              = function(self) return true end,
+    IsDebuff                = function(self) return true end,
+    IsBuff                  = function(self) return false end,
+    RemoveOnDeath           = function(self) return true end,
+    AllowIllusionDuplicate  = function(self) return false end,
+    GetEffectName           = function(self) return "particles/econ/items/huskar/huskar_2021_immortal/huskar_2021_immortal_burning_spear_debuff_flame_circulate.vpcf" end,
+    GetEffectAttachType     = function(self) return PATTACH_ABSORIGIN_FOLLOW end,
+})
+
+function modifier_ability_firecleave_fire:OnCreated()
+    self:StartIntervalThink(1)
+end
+
+function modifier_ability_firecleave_fire:OnIntervalThink()
+    local damage = self:GetAbility():GetSpecialValueFor("fire_damage")
+    local damageTable = {
+        victim = self:GetParent(),
+        attacker = self:GetCaster(),
+        damage = damage * self:GetParent():DamageHell(),
+        damage_type = self:GetAbility():GetAbilityDamageType(),
+        ability = self, --Optional.
+    }
+    ApplyDamage(damageTable)
 end
---
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_sargatanas/sunflame.lua
@@ -0,0 +1,87 @@
+ability_sunflame = class({})
+ 
+--------------------------------------------------------------------------------
+-- Ability Start
+function ability_sunflame:OnSpellStart()
+	-- unit identifier
+	local caster = self:GetCaster()
+	local target = self:GetCursorTarget()
+	local point = self:GetCursorPosition()
+
+	-- unit target just indicates point
+	if target then point = target:GetOrigin() end
+
+	local value1 = self:GetSpecialValueFor("some_value")
+		local damage = self:GetSpecialValueFor( "damage" )
+	local unduced = (self:GetSpecialValueFor( "unduced" )/100) + 1
+
+	-- load projectile
+	local projectile_name = "particles/units/heroes/hero_dragon_knight/dragon_knight_breathe_fire.vpcf"
+	local projectile_distance = self:GetSpecialValueFor( "range" )
+	local projectile_start_radius = self:GetSpecialValueFor( "start_radius" )
+	local projectile_end_radius = self:GetSpecialValueFor( "end_radius" )
+	local projectile_speed = self:GetSpecialValueFor( "speed" )
+	local projectile_direction = point - caster:GetOrigin()
+	projectile_direction.z = 0
+	projectile_direction = projectile_direction:Normalized()
+
+	-- create projectile
+	local info = {
+		Source = caster,
+		Ability = self,
+		vSpawnOrigin = caster:GetAbsOrigin(),
+		
+	    bDeleteOnHit = false,
+	    
+	    iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
+	    iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+	    
+	    EffectName = projectile_name,
+	    fDistance = projectile_distance,
+	    fStartRadius = projectile_start_radius,
+	    fEndRadius =projectile_end_radius,
+		vVelocity = projectile_direction * projectile_speed,
+		}
+ 
+	local projectile = ProjectileManager:CreateLinearProjectile(info)
+
+	-- register projectile data
+	self.projectiles[projectile] = {}
+	self.projectiles[projectile].damage = damage
+	self.projectiles[projectile].unduced = unduced
+
+
+	-- play effects
+	local sound_cast = "Hero_DragonKnight.BreathFire"
+	EmitSoundOn( sound_cast, caster )
+end
+
+ability_sunflame.projectiles = {}
+--------------------------------------------------------------------------------
+-- Projectile
+function ability_sunflame:OnProjectileHitHandle( target, location, handle )
+	if not target then return end
+
+	-- load data
+	local stack_overhell = self:GetSpecialValueFor( "stack_overhell" )
+
+	-- get data
+	local data = self.projectiles[handle]
+	local damage = data.damage
+
+	-- damage
+	local damageTable = {
+		victim = target,
+		attacker = self:GetCaster(),
+		damage = damage * target:DamageHell(),
+		damage_type = self:GetAbilityDamageType(),
+		ability = self, --Optional.
+	}
+
+	data.damage = damage * data.unduced
+
+	ApplyDamage(damageTable)
+
+    target:ModifierStackInc("modifier_overheating", stack_overhell,8,stack_overhell,self)
+
+end
