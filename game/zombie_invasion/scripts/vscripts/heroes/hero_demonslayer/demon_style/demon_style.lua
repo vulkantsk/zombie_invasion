be39cc129fb27ca84aa3ea85a90174abb5ba5d8f
Plarget <67307148+Plarget@users.noreply.github.com>
2023-05-09 22:40:04 +0400

    ghj

--- game/zombie_invasion/scripts/vscripts/heroes/hero_demonslayer/demon_style/demon_style.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_demonslayer/demon_style/demon_style.lua
@@ -1,11 +1,27 @@
 demon_style = class({})
 
 LinkLuaModifier( "modifier_demon_style", "heroes/hero_demonslayer/demon_style/demon_style", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_demon_style_attack", "heroes/hero_demonslayer/demon_style/demon_style", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_demon_style_count", "heroes/hero_demonslayer/demon_style/demon_style", LUA_MODIFIER_MOTION_NONE )
 
-    function demon_style:GetIntrinsicModifierName()
-        return "modifier_demon_style"
-    end
+
+function demon_style:GetIntrinsicModifierName()
+    return "modifier_demon_style"
+end
    
+    modifier_demon_style = class({
+        IsHidden                = function(self) return false end,
+        IsPurgable              = function(self) return false end,
+        IsDebuff                = function(self) return false end,
+        RemoveOnDeath           = function(self) return true end,
+        DeclareFunctions        = function(self) return 
+            {
+            MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+            MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+            MODIFIER_PROPERTY_PROCATTACK_FEEDBACK,
+             
+            } end,
+    })
 
     function modifier_demon_style:OnCreated( kv )
     self.stack_max = self:GetAbility():GetSpecialValueFor("stack_max")
@@ -19,46 +35,18 @@ LinkLuaModifier( "modifier_demon_style", "heroes/hero_demonslayer/demon_style/de
     self.stack_attackspeed = self:GetAbility():GetSpecialValueFor("stack_attackspeed")
     end
 
-
-    function modifier_demon_style:GetModifierPreAttack_BonusDamage()
-    return self:GetStackCount() * self.stack_damage
-    end
-
-
-
-    modifier_demon_style = class({
-        IsHidden                = function(self) return false end,
-        IsPurgable              = function(self) return false end,
-        IsDebuff                = function(self) return false end,
-        RemoveOnDeath           = function(self) return true end,
-        DeclareFunctions        = function(self) return 
-            {
-            MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
-            MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
-            MODIFIER_EVENT_ON_TAKEDAMAGE,
-            } end,
-    })
-
-modifier_demon_style = class({})
-
-    function modifier_demon_style:GetModifierProcAttack_Feedback( params )
+function modifier_demon_style:GetModifierProcAttack_Feedback( params )
     if IsServer() and (not self:GetParent():PassivesDisabled()) then
         -- filter enemy
-        local target = params.target        
-        local stack = 100
-        local count_stuck = 0
+        local target = params.target   
 
-        if RollPseudoRandomPercentage(stack, 1, self:GetCaster()) then 
-            count_stuck = count_stuck + 1              
-        end 
-        
-        if count_stuck >= 1 then 
-            self:AddStack( duration,count_stuck )
-            self:PlayEffects( params.target )  
-        end
-
+        self:AddStack( 5, 1 )
     end
 end
+
+    function modifier_demon_style:GetModifierPreAttack_BonusDamage()
+        return self:GetStackCount() * self.stack_damage
+    end
 
     -- Helper
 function modifier_demon_style:AddStack( duration, count )
@@ -66,11 +54,12 @@ function modifier_demon_style:AddStack( duration, count )
     local mod = self:GetParent():AddNewModifier(
         self:GetParent(),
         self:GetAbility(),
-        "modifier_demon_style",
+        "modifier_demon_style_count",
         {
-            duration = self.duration,
+            duration = duration,
         }
     )
+ 
     mod.modifier = self
     mod.bonus = count
     -- Add stack
@@ -78,7 +67,22 @@ function modifier_demon_style:AddStack( duration, count )
  
 end
 
-
 function modifier_demon_style:RemoveStack( value )
     self:SetStackCount( self:GetStackCount() - value )
 end
+
+modifier_demon_style_count = class({
+    IsHidden                 = function(self) return true end,
+    IsPurgable                 = function(self) return false end,
+    IsDebuff                 = function(self) return false end,
+    IsBuff                  = function(self) return true end,
+    RemoveOnDeath             = function(self) return false end,
+    GetAttributes                = function(self) return MODIFIER_ATTRIBUTE_MULTIPLE end,
+})
+ 
+function modifier_demon_style_count:OnRemoved()
+ 
+        self.modifier:RemoveStack(self.bonus)
+   
+end
+ 
