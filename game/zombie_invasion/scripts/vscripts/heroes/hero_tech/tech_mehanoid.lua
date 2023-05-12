59baf98849f464f93ca196c0e333d27ef9c2f851
Plarget <67307148+Plarget@users.noreply.github.com>
2023-05-12 18:11:46 +0400

    ds

--- game/zombie_invasion/scripts/vscripts/heroes/hero_tech/tech_mehanoid.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_tech/tech_mehanoid.lua
@@ -6,8 +6,9 @@ function tech_mehanoid:OnSpellStart()
 	local bonus_health = self:GetSpecialValueFor("bonus_health")
 	local bonus_damage = self:GetSpecialValueFor("bonus_damage")
 	local bonus_armor = self:GetSpecialValueFor("bonus_armor")
+	local bonus_regen = self:GetSpecialValueFor("bonus_regen")
 	local duration = self:GetSpecialValueFor("duration")
-
+	print(caster.meh)
 	if caster.meh and IsValidEntity(caster.meh) and caster.meh:IsAlive() then 
 		FindClearSpaceForUnit(caster.meh, point_for_unit, true)
 
@@ -15,13 +16,16 @@ function tech_mehanoid:OnSpellStart()
 		caster.meh:SetBaseDamageMin(bonus_damage)	
 		caster.meh:SetBaseDamageMax(bonus_damage)
 		caster.meh:SetPhysicalArmorBaseValue(bonus_armor)
+		caster.meh:SetBaseHealthRegen(bonus_regen)
 	elseif caster.meh and IsValidEntity(caster.meh) and not caster.meh:IsAlive() then 
 		FindClearSpaceForUnit(caster.meh, point_for_unit, true)
 		caster.meh:RespawnUnit()
 		
 	else 
-	caster.meh = CreateUnitByName("npc_mechanoid", point_for_unit, true, nil, nil, DOTA_TEAM_GOODGUYS)
  
+	caster.meh = CreateUnitByName("npc_mechanoid", point_for_unit, true, nil, nil, DOTA_TEAM_GOODGUYS)
+ 	
+ 	caster.meh:FindAbilityByName("mechanoid_splash"):SetLevel(1)
 	caster.meh:SetOwner( caster )
 	caster.meh:SetControllableByPlayer( caster:GetPlayerID(), true )
 	FindClearSpaceForUnit( caster.meh, point_for_unit, true )
@@ -29,6 +33,7 @@ function tech_mehanoid:OnSpellStart()
 	caster.meh:SetBaseDamageMin(bonus_damage)	
 	caster.meh:SetBaseDamageMax(bonus_damage)
 	caster.meh:SetPhysicalArmorBaseValue(bonus_armor)
+ 	caster.meh:SetBaseHealthRegen(bonus_regen)
 
 	end
 end
