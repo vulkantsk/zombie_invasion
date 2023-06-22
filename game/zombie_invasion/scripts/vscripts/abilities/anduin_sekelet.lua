3bcfa7dff4653fafb57aac0fb84e19477432d6a4
Plarget <67307148+Plarget@users.noreply.github.com>
2023-06-22 13:26:52 +0400

    ав

--- game/zombie_invasion/scripts/vscripts/abilities/anduin_sekelet.lua
+++ game/zombie_invasion/scripts/vscripts/abilities/anduin_sekelet.lua
@@ -2,7 +2,7 @@
 anduin_sekelet = class({})
  
 function anduin_sekelet:OnSpellStart()
-    for i=1,4 do
+    for i=1,6 do
        local unit = CreateUnitByName("npc_classic_big_skeleton_king", self:GetCaster():GetAbsOrigin(), true, nil, nil, DOTA_TEAM_BADGUYS)  
     end
      
