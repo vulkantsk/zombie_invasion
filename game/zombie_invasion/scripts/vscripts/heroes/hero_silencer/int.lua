2a46f993c20bedcbc564532c45006c9f3b34ce96
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2023-07-08 11:19:26 +0300

    F

---
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_silencer/int.lua
@@ -0,0 +1,30 @@
+LinkLuaModifier("modifier_int_buff", "heroes/hero_silencer/int", LUA_MODIFIER_MOTION_NONE)
+
+--------------------------------------------------------
+------------------------------------------------------------
+modifier_int_buff = class({
+	IsHidden 				= function(self) return false end,
+	IsPurgable 				= function(self) return false end,
+	IsDebuff 				= function(self) return false end,
+	IsBuff                  = function(self) return true end,
+	RemoveOnDeath 			= function(self) return false end,
+	DeclareFunctions		= function(self) return 
+		{MODIFIER_PROPERTY_STATS_INTELLECT_BONUS} end,
+})
+
+function modifier_int_buff:GetModifierBonusStats_Intellect()
+	return self:GetStackCount()*self:GetAbility():GetSpecialValueFor("grow_int")
+end
+
+--Increases the stack count of Flesh Heap.
+function StackCountIncrease( keys )
+    local caster = keys.caster
+    local ability = keys.ability
+    local fleshHeapStackModifier = "modifier_int_buff"
+    local currentStacks = caster:GetModifierStackCount(fleshHeapStackModifier, ability)
+
+	caster:AddNewModifier(caster, ability, fleshHeapStackModifier, nil)
+    caster:SetModifierStackCount(fleshHeapStackModifier, ability, (currentStacks + 1))
+end
+
+
--- game/zombie_invasion/scripts/vscripts/heroes/hero_tide/anchor_smash.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_tide/anchor_smash.lua
@@ -1,104 +1,43 @@
-LinkLuaModifier( "modifier_ability_tidehunter_anchor_smash_passive", "heroes/hero_tide/anchor_smash" ,LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_ability_tidehunter_anchor_smash", "heroes/hero_tide/anchor_smash" ,LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_ability_tidehunter_anchor_smash_reduction", "heroes/hero_tide/anchor_smash" ,LUA_MODIFIER_MOTION_NONE )
-
-if ability_tidehunter_anchor_smash == nil then
-    ability_tidehunter_anchor_smash = class({})
-end
+LinkLuaModifier( "modifier_anchor_smash_passive", "heroes/hero_tide/anchor_smash" ,LUA_MODIFIER_MOTION_NONE )
 
 
-function ability_tidehunter_anchor_smash:GetIntrinsicModifierName()
-    return "modifier_ability_tidehunter_anchor_smash_passive"
-end
+anchor_smash_passive = class({
 
-modifier_ability_tidehunter_anchor_smash_passive = class({})
-
-function modifier_ability_tidehunter_anchor_smash_passive:OnCreated()
-    self.chance = self:GetAbility():GetSpecialValueFor("chance")
-    local caster = self:GetCaster()
-    local duration = self:GetSpecialValueFor("reduction_duration")
-    local radius = self:GetSpecialValueFor("radius")
-
-    EmitSoundOn("Hero_Tidehunter.AnchorSmash", caster)
-
-    local all = FindUnitsInRadius(caster:GetTeam(), 
-    caster:GetOrigin(), 
-    nil, 
-    radius,
-    DOTA_UNIT_TARGET_TEAM_ENEMY, 
-    DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 
-    DOTA_UNIT_TARGET_FLAG_NONE,
-    FIND_ANY_ORDER, 
-    false)
-
-    caster:AddNewModifier(caster, self, "modifier_ability_tidehunter_anchor_smash", {})
-
-    for _, unit in ipairs(all) do
-        unit:AddNewModifier(caster, self, "modifier_ability_tidehunter_anchor_smash_reduction", {duration=duration})
-
-        caster:PerformAttack(unit, false, true, true, false, false, false, true)
-    end
-
-    caster:RemoveModifierByName("modifier_ability_tidehunter_anchor_smash")
-
-    local fx = ParticleManager:CreateParticle("particles/units/heroes/hero_tidehunter/tidehunter_anchor_hero.vpcf", PATTACH_ABSORIGIN, caster)
-    ParticleManager:SetParticleControl(fx, 0, caster:GetAbsOrigin())
-end
-
-function modifier_ability_tidehunter_anchor_smash_passive:OnAttackLanded(keys)
-    local target = keys.target
-    if self:GetCaster() == keys.attacker and not target:IsBuilding() then
-        if RollPseudoRandomPercentage(self.chance, 1, self:GetCaster()) then 
-              self:Smash()
-        end
-    end
-end
-
-modifier_ability_tidehunter_anchor_smash = class({
-    IsHidden                = function(self) return true end,
-    IsPurgable              = function(self) return false end,
-    IsPurgeException        = function(self) return false end,
-    IsDebuff                = function(self) return false end,
-    IsBuff                  = function(self) return true end,
-    RemoveOnDeath           = function(self) return true end,
-    DeclareFunctions        = function(self)
-        return {
-            MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
-            MODIFIER_PROPERTY_SUPPRESS_CLEAVE
-        }
-    end,
+    GetIntrinsicModifierName = function() return "modifier_anchor_smash_passive" end
 })
 
-function modifier_ability_tidehunter_anchor_smash:OnCreated()
-    self.attack_damage = self:GetAbility():GetSpecialValueFor("attack_damage")
+modifier_anchor_smash_passive = class({})
+
+function modifier_anchor_smash_passive:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_PROCATTACK_FEEDBACK,
+        MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+    }
 end
 
-function modifier_ability_tidehunter_anchor_smash:OnRefresh()
-    self:OnCreated()
-end
+function modifier_anchor_smash_passive:GetModifierProcAttack_Feedback()
 
-function modifier_ability_tidehunter_anchor_smash:GetModifierPreAttack_BonusDamage() return self.attack_damage end
-function modifier_ability_tidehunter_anchor_smash:GetSuppressCleave() return 1 end
-
-function modifier_ability_tidehunter_anchor_smash:Smash()
-
-
-
-     EmitSoundOn("Hero_Tidehunter.AnchorSmash", caster)
-end
-
-function modifier_ability_tidehunter_anchor_smash_reduction:OnCreated()
-    self.damage_reduction = self:GetAbility():GetSpecialValueFor("damage_reduction")
-
-    if IsServer() then
-        self.fx = ParticleManager:CreateParticle("particles/units/heroes/hero_tidehunter/tidehunter_anchor.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
-        ParticleManager:SetParticleControlEnt(self.fx, 0, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
-        self:AddParticle(self.fx, false, false, -1, false, false)
+    if RollPercentage(20) then
+        local enemies = FindUnitsInRadius(
+        self:GetParent():GetTeamNumber(), -- int, your team number
+        self:GetParent():GetOrigin(), -- point, center point
+        nil, -- handle, cacheUnit. (not known)
+        self.radius, -- float, radius. or use FIND_UNITS_EVERYWHERE
+        DOTA_UNIT_TARGET_TEAM_ENEMY, -- int, team filter
+        DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, -- int, type filter
+        self:GetAbility():GetAbilityTargetFlags(), -- int, flag filter
+        0, -- int, order filter
+        false -- bool, can grow cache
+        )
+        for _,enemy in pairs(enemies) do
+        ApplyDamage( {
+        enemy = target,
+        attacker = self:GetParent(),
+        bonus_damage = self.bonus_damage,
+        damage_type = DAMAGE_TYPE_PHYSICAL,
+        ability = self:GetAbility(), --Optional.
+        })
+        end
     end
-end
 
-function modifier_ability_tidehunter_anchor_smash_reduction:OnRefresh()
-    self:OnCreated()
 end
-
-function modifier_ability_tidehunter_anchor_smash_reduction:GetModifierBaseDamageOutgoing_Percentage() return self.damage_reduction end
