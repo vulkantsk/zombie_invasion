11eeee3fa36945c9d3fcfa203674e952789db8a9
Plarget <67307148+Plarget@users.noreply.github.com>
2023-07-21 01:07:17 +0400

    asd

--- game/zombie_invasion/scripts/vscripts/heroes/hero_sara/fragment_of_armor.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_sara/fragment_of_armor.lua
@@ -14,7 +14,7 @@ end
 
 
 modifier_sara_fragment_of_armor = class({
-	IsHidden = function() return true end,
+	IsHidden = function() return false end,
 	GetEffectAttachType = function() return PATTACH_ABSORIGIN_FOLLOW end,
 	GetEffectName = function() return "particles/arena/units/heroes/hero_sara/fragment_of_armor.vpcf" end,
 })
@@ -41,22 +41,3 @@ if IsServer() then
 		end
 	end
 end
-
-modifier_sara_fragment_of_armor = {
-		multiplier = function(attacker, victim, inflictor, damage)
-			local sara_fragment_of_armor = victim:FindAbilityByName("sara_fragment_of_armor")
-			
-				local blocked_damage_pct = sara_fragment_of_armor:GetAbilitySpecial("blocked_damage_pct") * 0.01
-				local mana_needed = (damage * blocked_damage_pct) / sara_fragment_of_armor:GetAbilitySpecial("damage_per_energy")
-				if victim:GetEnergy() >= mana_needed then
-					victim:EmitSound("Hero_Medusa.ManaShield.Proc")
-					victim:ModifyEnergy(-mana_needed)
-					local particleName = "particles/arena/units/heroes/hero_sara/fragment_of_armor_impact.vpcf"
-					local particle = ParticleManager:CreateParticle(particleName, PATTACH_ABSORIGIN_FOLLOW, victim)
-					ParticleManager:SetParticleControl(particle, 0, victim:GetAbsOrigin())
-					ParticleManager:SetParticleControl(particle, 1, Vector(mana_needed,0,0))
-					return 1 - blocked_damage_pct
-				end
-
-		end
-	}	
