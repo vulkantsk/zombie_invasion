0cfa3c2002e7bced1dfff2d1620bc0e476ab03c4
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2023-07-29 21:20:27 +0300

    F

--- game/zombie_invasion/scripts/vscripts/heroes/hero_templar/queen_of_the_secret_techiques/queen_of_the_secret_techiques.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_templar/queen_of_the_secret_techiques/queen_of_the_secret_techiques.lua
@@ -1,133 +1,85 @@
-lion_finger_of_death_lua = class({})
-LinkLuaModifier( "modifier_lion_finger_of_death_lua", "heroes/hero_lion/finger/modifier_lion_finger_of_death_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_templar_secret", "heroes/hero_templar/queen_of_the_secret_techiques/queen_of_the_secret_techiques", LUA_MODIFIER_MOTION_NONE )
+
+
+
+templar_secret = class({})
+
+function templar_secret:GetIntrinsicModifierName()
+    return "modifier_templar_secret"
+end
+
+
+modifier_templar_secret = class({})
 
 --------------------------------------------------------------------------------
--- Custom KV
--- AOE Radius
-function lion_finger_of_death_lua:GetAOERadius()
- 
-		return self:GetSpecialValueFor( "splash_radius_scepter" )
-	 
-
-	 
+-- Classifications
+function modifier_templar_secret:IsHidden()
+	return true
 end
 
-function lion_finger_of_death_lua:GetIntrinsicModifierName()
-	return "modifier_lion_finger_of_death_lua"
-end
-
-function lion_finger_of_death_lua:GetCooldown( level )
-	if self:GetCaster():HasScepter() then
-		return self:GetSpecialValueFor( "cooldown_scepter" )
-	end
-
-	return self.BaseClass.GetCooldown( self, level )
-end
-
-function lion_finger_of_death_lua:GetManaCost( level )
-	if self:GetCaster():HasScepter() then
-		return self:GetSpecialValueFor( "mana_cost_scepter" )
-	end
-
-	return self.BaseClass.GetManaCost( self, level )
-end
-
- function lion_finger_of_death_lua:OnOwnerDied()
- 		if IsServer() then       
- 
-end
- end
-
---------------------------------------------------------------------------------
--- Ability Start
-function lion_finger_of_death_lua:OnSpellStart()
-	-- unit identifier
-	local caster = self:GetCaster()
-	local target = self:GetCursorTarget()
-
-	-- pre-effects
-	local sound_cast = "Hero_Lion.FingerOfDeath"
-	EmitSoundOn( sound_cast, caster )
-
-	-- cancel if linken
-	if target:TriggerSpellAbsorb(self) then
-		self:PlayEffects( target )
-		return 
-	end
-
-	-- load data
-
-	local delay = self:GetSpecialValueFor("damage_delay")
-	local search = self:GetSpecialValueFor("splash_radius_scepter")
-
-	-- find targets
-	local targets = {}
- 
-		targets = FindUnitsInRadius(
-			caster:GetTeamNumber(),	-- int, your team number
-			target:GetOrigin(),	-- point, center point
-			nil,	-- handle, cacheUnit. (not known)
-			search,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-			0,	-- int, flag filter
-			0,	-- int, order filter
-			false	-- bool, can grow cache
-		)
- 
-
-	for _,enemy in pairs(targets) do
-		-- delay
-		enemy:AddNewModifier(
-			caster, -- player source
-			self, -- ability source
-			"modifier_lion_finger_of_death_lua", -- modifier name
-			{ duration = delay } -- kv
-		)
-
-		-- effects
-		self:PlayEffects( enemy )
-	end
+function modifier_templar_secret:IsPurgable()
+	return false
 end
 
 --------------------------------------------------------------------------------
-function lion_finger_of_death_lua:PlayEffects( target )
-	-- Get Resources
-	local particle_cast = "particles/units/heroes/hero_lion/lion_spell_finger_of_death.vpcf"
-	local sound_cast = "Hero_Lion.FingerOfDeathImpact"
+function modifier_templar_secret:OnIntervalThink()
+	if not IsServer() then return end
+	if self:GetAbility():IsFullyCastable() then
+		self:GetAbility().params.record = true
+	end	
+end
 
-	-- load data
-	local caster = self:GetCaster()
-	local direction = (caster:GetOrigin()-target:GetOrigin()):Normalized()
+-- Initializations
+function modifier_templar_secret:OnCreated( kv )
+	-- references
+	self.temp_crit_chance = self:GetAbility():GetSpecialValueFor( "temp_crit_chance" )
+	self.temp_crit_mult = self:GetAbility():GetSpecialValueFor( "temp_crit_mult" )
+end
 
-	-- Create Particle
-	-- local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN, caster )
-	local effect_cast = assert(loadfile("heroes/rubick_spell_steal_lua/rubick_spell_steal_lua_arcana"))(self, particle_cast, PATTACH_ABSORIGIN, caster )
-	local attach = "attach_attack1"
-	if caster:ScriptLookupAttachment( "attach_attack2" )~=0 then attach = "attach_attack2" end
-	ParticleManager:SetParticleControlEnt(
-		effect_cast,
-		0,
-		caster,
-		PATTACH_POINT_FOLLOW,
-		attach,
-		Vector(0,0,0), -- unknown
-		true -- unknown, true
-	)
-	ParticleManager:SetParticleControlEnt(
-		effect_cast,
-		1,
-		target,
-		PATTACH_POINT_FOLLOW,
-		"attach_hitloc",
-		Vector(0,0,0), -- unknown
-		true -- unknown, true
-	)
-	ParticleManager:SetParticleControl( effect_cast, 2, target:GetOrigin() )
-	ParticleManager:SetParticleControl( effect_cast, 3, target:GetOrigin() + direction )
-	ParticleManager:SetParticleControlForward( effect_cast, 3, -direction )
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+function modifier_templar_secret:OnRefresh( kv )
+	-- references
+	self.temp_crit_chance = self:GetAbility():GetSpecialValueFor( "temp_crit_chance" )
+	self.temp_crit_mult = self:GetAbility():GetSpecialValueFor( "temp_crit_mult" )
+end
 
-	-- Create Sound
-	EmitSoundOn( sound_cast, target )
+function modifier_templar_secret:OnDestroy( kv )
+
+end
+
+--------------------------------------------------------------------------------
+-- Modifier Effects
+function modifier_templar_secret:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE,
+		MODIFIER_PROPERTY_PROCATTACK_FEEDBACK,
+		MODIFIER_PROPERTY_PROJECTILE_NAME,
+	}
+
+	return funcs
+end
+function modifier_templar_secret:GetModifierPreAttack_CriticalStrike( params )
+	
+		if params.target:GetTeamNumber()==self:GetParent():GetTeamNumber() then
+			return
+		end
+
+		-- Throw dice
+		if RandomInt(0, 100)<self.temp_crit_chance then
+			self.record = params.record
+			return self.temp_crit_mult
+		end
+	
+end
+function modifier_templar_secret:GetModifierProcAttack_Feedback( params )
+	if IsServer() then
+		if self.record and self.record == params.record then
+
+			self.record = nil
+			
+
+			-- Play effects
+			local sound_cast = "Hero_TemplarAssassin.Meld.Attack"
+			EmitSoundOn( sound_cast, params.target )
+		end
+	end
 end
