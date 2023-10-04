97fe1de92df0b1508f70c8a101e3cde77e8c24f1
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2023-10-04 19:55:27 +0300

    F

---
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_blood_hunter/bloodstained_memory/bloodstained_memory.lua
@@ -0,0 +1,48 @@
+LinkLuaModifier( "modifier_bloodrage_buff", "heroes/hero_blood_hunter/bloodrage/bloodrage", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_bloodrage_count", "heroes/hero_blood_hunter/bloodrage/bloodrage", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_bloodstained_memory", "heroes/hero_blood_hunter/bloodstained_memory/bloodstained_memory", LUA_MODIFIER_MOTION_NONE )
+
+bloodstained_memory = class({})
+
+function bloodstained_memory:GetIntrinsicModifierName()
+    return "modifier_bloodstained_memory"
+end
+
+ modifier_bloodstained_memory = class({
+        IsHidden                = function(self) return false end,
+        IsPurgable              = function(self) return false end,
+        IsDebuff                = function(self) return false end,
+        RemoveOnDeath           = function(self) return true end,
+        DeclareFunctions        = function(self) return 
+            {
+            MODIFIER_PROPERTY_EXTRA_HEALTH_BONUS,
+            MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+            MODIFIER_PROPERTY_IGNORE_MOVESPEED_LIMIT,
+            } end,
+    })
+
+
+
+function modifier_bloodstained_memory:OnCreated()
+    self.healthbonus = self:GetAbility():GetSpecialValueFor("healthbonus")
+    self.movespeed = self:GetAbility():GetSpecialValueFor("movespeed")
+end
+
+function modifier_bloodstained_memory:OnRefresh()
+    self:OnCreated()
+
+end
+
+function modifier_bloodstained_memory:GetModifierIgnoreMovespeedLimit()  
+    return 1
+end
+
+    function modifier_bloodstained_memory:GetModifierExtraHealthBonus()
+        return self.healthbonus * (self:GetParent():FindModifierByName("modifier_bloodrage_buff"):GetStackCount() + (0.25 / self.GetParent():GetHealth()))
+    end
+
+    function modifier_bloodstained_memory:GetModifierMoveSpeedBonus_Constant()
+        return self.movespeed * self:GetParent():FindModifierByName("modifier_bloodrage_buff"):GetStackCount()
+    end
+
+
