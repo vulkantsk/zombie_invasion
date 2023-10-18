2af203156145a705975b9bfb99774cb585a36e7e
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2023-10-18 15:13:33 +0300

    F

---
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_smaug/fire_punishment/fire_punishment.lua
@@ -0,0 +1,257 @@
+LinkLuaModifier( "modifier_fire_punishment_orb", "heroes/hero_smaug/fire_punishment/fire_punishment", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_fire_punishment_fire", "heroes/hero_smaug/fire_punishment/fire_punishment", LUA_MODIFIER_MOTION_NONE )
+
+fire_punishment = {}
+
+function fire_punishment:GetIntrinsicModifierName()
+    return "modifier_fire_punishment_orb"
+end
+
+function fire_punishment:GetProjectileName()
+    return "particles/units/heroes/hero_jakiro/jakiro_base_attack_fire.vpcf"
+end
+
+function fire_punishment:OnOrbFire( params )
+    EmitSoundOn( "Hero_Jakiro.LiquidFire", self:GetCaster() )
+end
+
+function fire_punishment:OnOrbImpact( params )
+    local caster = self:GetCaster()
+    local duration = self:GetDuration()
+    local radius = self:GetSpecialValueFor( "radius" )
+
+    local enemies = FindUnitsInRadius(
+        caster:GetTeamNumber(),
+        params.target:GetOrigin(),
+        nil,
+        radius,
+        DOTA_UNIT_TARGET_TEAM_ENEMY,
+        DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_BUILDING,
+        0,
+        0,
+        false
+    )
+
+    for _,enemy in pairs(enemies) do
+        enemy:AddNewModifier(
+            caster,
+            self,
+            "modifier_fire_punishment_fire",
+            { duration = duration }
+        )
+        
+    end
+
+    local effect_cast = ParticleManager:CreateParticle(
+        "particles/units/heroes/hero_jakiro/jakiro_liquid_fire_explosion.vpcf",
+        PATTACH_ABSORIGIN_FOLLOW,
+        params.target
+    )
+    ParticleManager:SetParticleControl( effect_cast, 1, Vector( radius, radius, radius ) )
+    ParticleManager:ReleaseParticleIndex( effect_cast )
+
+    EmitSoundOn( "Hero_Jakiro.LiquidFire", self:GetCaster() )
+end
+
+modifier_fire_punishment_fire = {}
+
+function modifier_fire_punishment_fire:IsHidden()
+    return false
+end
+
+function modifier_fire_punishment_fire:IsDebuff()
+    return true
+end
+
+function modifier_fire_punishment_fire:IsStunDebuff()
+    return false
+end
+
+function modifier_fire_punishment_fire:IsPurgable()
+    return true
+end
+
+function modifier_fire_punishment_fire:OnCreated( kv )
+    local damage = self:GetAbility():GetSpecialValueFor( "damage" )
+    self.debuff_magic = self:GetAbility():GetSpecialValueFor( "debuff_magic" )
+
+    if not IsServer() then return end
+
+    self.damageTable = {
+        victim = self:GetParent(),
+        attacker = self:GetCaster(),
+        damage = damage,
+        damage_type = self:GetAbility():GetAbilityDamageType(),
+        ability = self:GetAbility(),
+    }
+
+    self:StartIntervalThink( 0.5 )
+end
+
+function modifier_fire_punishment_fire:OnRefresh( kv )
+    local damage = self:GetAbility():GetSpecialValueFor( "damage" )
+    self.debuff_magic = self:GetAbility():GetSpecialValueFor( "debuff_magic" )
+
+    if not IsServer() then return end
+
+    self.damageTable.damage = damage    
+end
+
+function modifier_fire_punishment_fire:DeclareFunctions()
+    return { MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS }
+end
+
+function modifier_fire_punishment_fire:GetModifierMagicalResistanceBonus()
+    return self.debuff_magic
+end
+
+function modifier_fire_punishment_fire:OnIntervalThink()
+    ApplyDamage( self.damageTable )
+end
+
+function modifier_fire_punishment_fire:GetEffectName()
+    return "particles/units/heroes/hero_jakiro/jakiro_liquid_fire_debuff.vpcf"
+end
+
+function modifier_fire_punishment_fire:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+modifier_fire_punishment_orb = {}
+
+function modifier_fire_punishment_orb:IsHidden()
+    return true
+end
+
+function modifier_fire_punishment_orb:IsDebuff()
+    return false
+end
+
+function modifier_fire_punishment_orb:IsPurgable()
+    return false
+end
+
+function modifier_fire_punishment_orb:GetAttributes()
+    return MODIFIER_ATTRIBUTE_PERMANENT
+end
+
+function modifier_fire_punishment_orb:OnCreated( kv )
+    self.ability = self:GetAbility()
+    self.cast = false
+    self.records = {}
+end
+
+function modifier_fire_punishment_orb:DeclareFunctions()
+    return {
+        MODIFIER_EVENT_ON_ATTACK_LANDED,
+        MODIFIER_PROPERTY_PROCATTACK_FEEDBACK,
+        MODIFIER_EVENT_ON_ATTACK_RECORD_DESTROY,
+        MODIFIER_EVENT_ON_ORDER,
+        MODIFIER_PROPERTY_PROJECTILE_NAME
+    }
+end
+
+function modifier_fire_punishment_orb:OnAttackLanded( params )
+    if params.attacker ~= self:GetParent() then return end
+
+    if self:ShouldLaunch( params.target ) then
+
+        self.records[params.record] = true
+
+        if self.ability.OnOrbFire then self.ability:OnOrbFire( params ) end
+    end
+
+    self.cast = false
+end
+
+function modifier_fire_punishment_orb:GetModifierProcAttack_Feedback( params )
+        self.ability:OnOrbImpact( params )
+end
+
+
+function modifier_fire_punishment_orb:OnAttackRecordDestroy( params )
+    self.records[params.record] = nil
+end
+
+function modifier_fire_punishment_orb:OnOrder( params )
+    if params.unit~=self:GetParent() then return end
+
+    if params.ability then
+        if params.ability==self:GetAbility() then
+            self.cast = true
+            return
+        end
+
+        local pass = false
+        local behavior = params.ability:GetBehavior()
+        if self:FlagExist( behavior, DOTA_ABILITY_BEHAVIOR_DONT_CANCEL_CHANNEL ) or 
+            self:FlagExist( behavior, DOTA_ABILITY_BEHAVIOR_DONT_CANCEL_MOVEMENT ) or
+            self:FlagExist( behavior, DOTA_ABILITY_BEHAVIOR_IGNORE_CHANNEL )
+        then
+            local pass = true
+        end
+
+        if self.cast and (not pass) then
+            self.cast = false
+        end
+    else
+        if self.cast then
+            if self:FlagExist( params.order_type, DOTA_UNIT_ORDER_MOVE_TO_POSITION ) or
+                self:FlagExist( params.order_type, DOTA_UNIT_ORDER_MOVE_TO_TARGET ) or
+                self:FlagExist( params.order_type, DOTA_UNIT_ORDER_ATTACK_MOVE ) or
+                self:FlagExist( params.order_type, DOTA_UNIT_ORDER_ATTACK_TARGET ) or
+                self:FlagExist( params.order_type, DOTA_UNIT_ORDER_STOP ) or
+                self:FlagExist( params.order_type, DOTA_UNIT_ORDER_HOLD_POSITION )
+            then
+                self.cast = false
+            end
+        end
+    end
+end
+
+function modifier_fire_punishment_orb:GetModifierProjectileName()
+    if not self.ability.GetProjectileName then return end
+
+    if self:ShouldLaunch( self:GetCaster():GetAggroTarget() ) then
+        return self.ability:GetProjectileName()
+    end
+end
+
+function modifier_fire_punishment_orb:ShouldLaunch( target )
+    if self.ability:GetAutoCastState() then
+        if self.ability.CastFilterResultTarget~=CDOTA_Ability_Lua.CastFilterResultTarget then
+            if self.ability:CastFilterResultTarget( target )==UF_SUCCESS then
+                self.cast = true
+            end
+        else
+            local nResult = UnitFilter(
+                target,
+                self.ability:GetAbilityTargetTeam(),
+                self.ability:GetAbilityTargetType(),
+                self.ability:GetAbilityTargetFlags(),
+                self:GetCaster():GetTeamNumber()
+            )
+            if nResult == UF_SUCCESS then
+                self.cast = true
+            end
+        end
+    end
+
+    if self.cast and self.ability:IsFullyCastable() and (not self:GetParent():IsSilenced()) then
+        return true
+    end
+
+    return false
+end
+
+function modifier_fire_punishment_orb:FlagExist(a,b)
+    a=tonumber(a)
+    b=tonumber(b)
+    local p,c,d=1,0,b
+    while a>0 and b>0 do
+        local ra,rb=a%2,b%2
+        if ra+rb>1 then c=c+p end
+        a,b,p=(a-ra)/2,(b-rb)/2,p*2
+    end
+    return c==d
+end
---
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_smaug/incandescent_fury/incandescent_fury.lua
@@ -0,0 +1,199 @@
+LinkLuaModifier( "modifier_incandescent_fury", "heroes/hero_smaug/incandescent_fury/incandescent_fury", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_incandescent_fury_thinker", "heroes/hero_smaug/incandescent_fury/incandescent_fury", LUA_MODIFIER_MOTION_NONE )
+
+incandescent_fury = {}
+
+function incandescent_fury:GetCastRange( vLocation, hTarget )
+    if self:GetCaster():HasScepter() then
+        return self:GetSpecialValueFor( "cast_range_scepter" )
+    end
+
+    return self:GetSpecialValueFor( "cast_range" )
+end
+
+function incandescent_fury:OnSpellStart()
+    local caster = self:GetCaster()
+    local point = self:GetCursorPosition()
+
+    local dir = point - caster:GetAbsOrigin()
+    dir.z = 0
+    dir = dir:Normalized()
+
+    local duration = self:GetSpecialValueFor( "duration" )
+    if caster:HasScepter() then
+        duration = self:GetSpecialValueFor( "duration_scepter" )
+    end
+
+    CreateModifierThinker(
+        caster,
+        self,
+        "modifier_incandescent_fury_thinker",
+        {
+            duration = duration,
+            x = dir.x,
+            y = dir.y,
+        },
+        caster:GetAbsOrigin(),
+        caster:GetTeamNumber(),
+        false
+    )
+
+    EmitSoundOn( "Hero_Jakiro.Macropyre.Cast", caster )
+end
+
+modifier_incandescent_fury = {}
+
+function modifier_incandescent_fury:IsHidden()
+    return false
+end
+
+function modifier_incandescent_fury:IsDebuff()
+    return true
+end
+
+function modifier_incandescent_fury:IsStunDebuff()
+    return false
+end
+
+function modifier_incandescent_fury:IsPurgable()
+    return false
+end
+
+function modifier_incandescent_fury:OnCreated( kv )
+    if not IsServer() then return end
+
+    self.damageTable = {
+        victim = self:GetParent(),
+        attacker = self:GetCaster(),
+        damage = kv.damage,
+        damage_type = kv.damage_type,
+        ability = self:GetAbility(),
+    }
+
+    self:StartIntervalThink( kv.interval )
+end
+
+function modifier_incandescent_fury:OnRefresh( kv )
+    if not IsServer() then return end
+    self.damageTable.damage = kv.damage
+    self.damageTable.damage_type = kv.damage_type
+end
+
+function modifier_incandescent_fury:OnIntervalThink()
+    ApplyDamage( self.damageTable )
+end
+
+function modifier_incandescent_fury:GetEffectName()
+    return "particles/units/heroes/hero_jakiro/jakiro_liquid_fire_debuff.vpcf"
+end
+
+function modifier_incandescent_fury:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+modifier_incandescent_fury_thinker = {}
+
+function modifier_incandescent_fury_thinker:IsHidden()
+    return false
+end
+
+function modifier_incandescent_fury_thinker:IsDebuff()
+    return false
+end
+
+function modifier_incandescent_fury_thinker:IsStunDebuff()
+    return false
+end
+
+function modifier_incandescent_fury_thinker:IsPurgable()
+    return false
+end
+
+function modifier_incandescent_fury_thinker:OnCreated( kv )
+    self.caster = self:GetCaster()
+    self.parent = self:GetParent()
+    self.radius = self:GetAbility():GetSpecialValueFor( "path_radius" )
+    self.duration = self:GetAbility():GetSpecialValueFor( "linger_duration" )
+    self.interval = self:GetAbility():GetSpecialValueFor( "burn_interval" )
+    self.range = self:GetAbility():GetCastRange( self.parent:GetAbsOrigin(), nil ) + self.caster:GetCastRangeBonus()
+    if self:GetCaster():HasScepter() then
+        self.damage = self:GetAbility():GetSpecialValueFor( "damage_scepter" )
+    else
+        self.damage = self:GetAbility():GetSpecialValueFor( "damage" )
+    end
+
+    if not IsServer() then return end
+
+    self.abilityDamageType = self:GetAbility():GetAbilityDamageType()
+    self.abilityTargetTeam = self:GetAbility():GetAbilityTargetTeam()
+    self.abilityTargetType = self:GetAbility():GetAbilityTargetType()
+    self.abilityTargetFlags = self:GetAbility():GetAbilityTargetFlags()
+
+    local start_range = 234
+    self.direction = Vector( kv.x, kv.y, 0 )
+    self.startpoint = self.parent:GetAbsOrigin() + self.direction * start_range
+    self.endpoint = self.startpoint + self.direction * self.range
+
+    local step = 0
+    while step < self.range do
+        local loc = self.startpoint + self.direction * step
+        GridNav:DestroyTreesAroundPoint( loc, self.radius, true )
+
+        step = step + self.radius
+    end
+
+    self:StartIntervalThink( self.interval )
+
+    local duration = self:GetDuration()
+    local effect_cast = ParticleManager:CreateParticle(
+        "particles/units/heroes/hero_jakiro/jakiro_macropyre.vpcf",
+        PATTACH_WORLDORIGIN,
+        self.parent
+    )
+    ParticleManager:SetParticleControl( effect_cast, 0, self.startpoint )
+    ParticleManager:SetParticleControl( effect_cast, 1, self.endpoint )
+    ParticleManager:SetParticleControl( effect_cast, 2, Vector( duration, 0, 0 ) )
+
+    self:AddParticle(
+        effect_cast,
+        false,
+        false,
+        -1,
+        false,
+        false
+    )
+
+    EmitSoundOn( "hero_jakiro.macropyre", self.parent )
+end
+
+function modifier_incandescent_fury_thinker:OnDestroy()
+    if not IsServer() then return end
+    UTIL_Remove( self:GetParent() )
+end
+
+function modifier_incandescent_fury_thinker:OnIntervalThink()
+    local enemies = FindUnitsInLine(
+        self.caster:GetTeamNumber(),
+        self.startpoint,
+        self.endpoint,
+        nil,
+        self.radius,
+        self.abilityTargetTeam,
+        self.abilityTargetType,
+        self.abilityTargetFlags
+    )
+
+    for _,enemy in pairs(enemies) do
+        enemy:AddNewModifier(
+            self.caster,
+            self:GetAbility(),
+            "modifier_incandescent_fury",
+            {
+                duration = self.duration,
+                interval = self.interval,
+                damage = self.damage * self.interval,
+                damage_type = self.abilityDamageType,
+            }
+        )
+    end
+end
