74732c514aec6068bdd83cf1b2c04bdc53b9cd06
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2024-02-12 17:45:59 +0300

    А

--- game/zombie_invasion/scripts/vscripts/items/item_gold.lua
+++ game/zombie_invasion/scripts/vscripts/items/item_gold.lua
@@ -1,55 +1,55 @@
 item_gold = class({
 })
 function item_gold:OnSpellStart()
-    self:GetCaster():SetGold(self:GetCaster():GetGold() + 150, false)
+    self:GetCaster():SetGold(self:GetCaster():GetGold() + 300, false)
     self:GetCaster():RemoveItem(self)
 end
 
 item_gold2 = class({
 })
 function item_gold2:OnSpellStart()
-    self:GetCaster():SetGold(self:GetCaster():GetGold() + 300, false)
+    self:GetCaster():SetGold(self:GetCaster():GetGold() + 600, false)
     self:GetCaster():RemoveItem(self)
 end
 
 item_gold3 = class({
 })
 function item_gold3:OnSpellStart()
-    self:GetCaster():SetGold(self:GetCaster():GetGold() + 450, false)
+    self:GetCaster():SetGold(self:GetCaster():GetGold() + 900, false)
     self:GetCaster():RemoveItem(self)
 end
 
 item_gold4 = class({
 })
 function item_gold4:OnSpellStart()
-    self:GetCaster():SetGold(self:GetCaster():GetGold() + 600, false)
+    self:GetCaster():SetGold(self:GetCaster():GetGold() + 1200, false)
     self:GetCaster():RemoveItem(self)
 end
 
 item_exp = class({
 })
 function item_exp:OnSpellStart()
-    self:GetCaster():AddExperience(150, 0, false, true)
+    self:GetCaster():AddExperience(400, 0, false, true)
     self:GetCaster():RemoveItem(self)
 end
 
 item_exp2 = class({
 })
 function item_exp2:OnSpellStart()
-    self:GetCaster():AddExperience(300, 0, false, true)
+    self:GetCaster():AddExperience(600, 0, false, true)
     self:GetCaster():RemoveItem(self)
 end
 
 item_exp3 = class({
 })
 function item_exp3:OnSpellStart()
-    self:GetCaster():AddExperience(450, 0, false, true)
+    self:GetCaster():AddExperience(800, 0, false, true)
     self:GetCaster():RemoveItem(self)
 end
 
 item_exp4 = class({
 })
 function item_exp4:OnSpellStart()
-    self:GetCaster():AddExperience(600, 0, false, true)
+    self:GetCaster():AddExperience(1200, 0, false, true)
     self:GetCaster():RemoveItem(self)
 end
