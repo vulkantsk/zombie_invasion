1f9e36216e6115dd3b7e73a58b2ecfb30fe3666e
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2024-02-18 19:53:39 +0300

    F

--- game/zombie_invasion/scripts/vscripts/heroes/hero_samurai/spells/death_jug.lua
+++ game/zombie_invasion/scripts/vscripts/heroes/hero_samurai/spells/death_jug.lua
@@ -3,27 +3,16 @@ LinkLuaModifier("modifier_phantom_assassin_death_rush_passive", "heroes/hero_sam
 LinkLuaModifier("modifier_phantom_assassin_jug_death", "heroes/hero_samurai/spells/death_jug", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_phantom_assassin_death_rush_cooldown", "heroes/hero_samurai/spells/death_jug", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_phantom_assassin_jug_death_after", "heroes/hero_samurai/spells/death_jug", LUA_MODIFIER_MOTION_NONE)
-  
+LinkLuaModifier("modifier_phantom_assassin_death_rush_buff", "heroes/hero_samurai/spells/death_jug", LUA_MODIFIER_MOTION_NONE)
 jugger_dead = class({})
 
-function jugger_dead:OnAbilityPhaseStart()
-    if IsServer() then
-        EmitSoundOn("gate_dead",  self:GetCaster())
-      self:GetCaster():StartGestureWithPlaybackRate(ACT_DOTA_VICTORY, 3.00)  
-  end
-  return true
-   end
-function jugger_dead:OnAbilityPhaseInterrupted()
-    if IsServer() then
-      StopSoundOn("gate_dead", self:GetCaster())     
-      self:GetCaster():RemoveGesture(ACT_DOTA_VICTORY) 
-  end
-  return true
+function jugger_dead:GetIntrinsicModifierName()
+    return "modifier_phantom_assassin_death_rush_buff"
 end
 
 function jugger_dead:OnSpellStart()
 
-    self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_phantom_assassin_death_rush", { duration =  self:GetSpecialValueFor("duration")})
+    self:GetCaster():AddNewModifier(self:GetCaster(), nil, "modifier_phantom_assassin_death_rush", {duration = 30})
             EmitSoundOn("gate_dead_theme",  self:GetCaster())
      
     self:PlayEffects()
@@ -82,37 +71,21 @@ modifier_phantom_assassin_death_rush = class({
             MODIFIER_PROPERTY_TURN_RATE_PERCENTAGE,
             MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
             MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
-        MODIFIER_PROPERTY_IGNORE_MOVESPEED_LIMIT,
             MODIFIER_PROPERTY_MIN_HEALTH,
         } end,
-    CheckState      = function(self) return 
-        {
-        
-  
-        } end,
-})
-   function  modifier_phantom_assassin_death_rush:OnCreated()
-  
-   
-        self:StartIntervalThink( 0.2 )
+    })
+function  modifier_phantom_assassin_death_rush:OnCreated()
+    self:StartIntervalThink( 0.2 )
   
 end
  
-    function  modifier_phantom_assassin_death_rush:OnIntervalThink()
-    if IsServer() then
-   
-                local damageTable = {
-            victim = self:GetCaster(),
-            attacker = self:GetCaster(),
-            damage = (self:GetCaster():GetMaxHealth()*(self:GetAbility():GetSpecialValueFor("damage_self")/100)) *0.2,
-            damage_type = DAMAGE_TYPE_PURE,  
-            ability = self:GetAbility(), --Optional.
-        }
-        ApplyDamage(damageTable)
- end
+function  modifier_phantom_assassin_death_rush:OnIntervalThink()
+        if IsServer() then
+            self:GetParent():SetHealth(math.max( self:GetParent():GetHealth() - (100 * 8.25), 1))
+        end
 end
 
- function modifier_phantom_assassin_death_rush:GetModifierModelChange()
+function modifier_phantom_assassin_death_rush:GetModifierModelChange()
     return "models/items/warlock/golem/puppet_summoner_golem/puppet_summoner_golem.vmdl"
 end
 
@@ -121,106 +94,21 @@ end
 function modifier_phantom_assassin_death_rush:OnDestroy()
        local caster = self:GetCaster()
        local stack =  caster:GetModifierStackCount("modifier_phantom_assassin_jug_death_after", nil)
- 
         caster:AddNewModifier(caster, self, "modifier_phantom_assassin_jug_death_after", { })
- 
-
-            caster:SetModifierStackCount("modifier_phantom_assassin_jug_death_after", nil, (stack + 1))
+        caster:SetModifierStackCount("modifier_phantom_assassin_jug_death_after", nil, (stack + 1))
         caster:AddNewModifier(caster, self, "modifier_phantom_assassin_jug_death_after", { })
-         caster:AddNewModifier(caster, self, "modifier_phantom_assassin_jug_death", { duration = 10})
- 
 end
-
-function modifier_phantom_assassin_death_rush:GetModifierIgnoreMovespeedLimit()  
-    return 1
-end
-
 
 function modifier_phantom_assassin_death_rush:GetMinHealth()
     return 1
 end
-
-function modifier_phantom_assassin_death_rush:GetModifierStatusResistanceStacking( params )
- 
-        return self:GetAbility():GetSpecialValueFor("status_resist")
- 
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierPercentageCooldown()
-    return self:GetAbility():GetSpecialValueFor("cooldown")
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierTurnRate_Percentage()
-    return self:GetAbility():GetSpecialValueFor("turn")
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierBaseAttackTimeConstant()
-    return self:GetAbility():GetSpecialValueFor("attack_base")
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierPercentageCasttime()
-    return 100
-end
-
-
-function modifier_phantom_assassin_death_rush:GetModifierBaseAttack_BonusDamage()
-    return self:GetAbility():GetSpecialValueFor("dmg_bonus")
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierMoveSpeedBonus_Constant()
-    return self:GetAbility():GetSpecialValueFor("ms_bonus")
-end
-
-function modifier_phantom_assassin_death_rush:GetModifierAttackSpeedBonus_Constant()
-    return self:GetAbility():GetSpecialValueFor("as_bonus")
-end
- 
- 
  
 function modifier_phantom_assassin_death_rush:GetEffectName()
     return "particles/heroes/death/samurai_golem_ambient.vpcf"
 end
 
- 
-
- modifier_phantom_assassin_jug_death = class({
+modifier_phantom_assassin_jug_death_after = class({
     IsHidden                = function(self) return true end,
-    IsPurgable              = function(self) return false end,
-    IsDebuff                = function(self) return false end,
-    IsBuff                  = function(self) return true end,
-    RemoveOnDeath           = function(self) return true end,
-    DeclareFunctions        = function(self) return 
-        {
- 
-        } end,
-    CheckState      = function(self) return 
-        {
-                  
-            [MODIFIER_STATE_INVULNERABLE] = true, 
-            [MODIFIER_STATE_ROOTED] = true, 
-            [MODIFIER_STATE_DISARMED] = true, 
-            [MODIFIER_STATE_STUNNED] = true, 
- 
-        } end,
-})
-
-       function  modifier_phantom_assassin_jug_death:OnCreated()
-               EmitSoundOn("gate_dead_after",  self:GetCaster())     
- 
-end  
-
- 
-
-function modifier_phantom_assassin_jug_death:OnDestroy()
- 
-self:GetCaster():ForceKill(false)
- 
- 
-     
-end
-
- modifier_phantom_assassin_jug_death_after = class({
-    IsHidden                = function(self) return false end,
     IsPurgable              = function(self) return false end,
     IsDebuff                = function(self) return true end,
     IsBuff                  = function(self) return false end,
@@ -232,13 +120,9 @@ end
  
 })
 
-       function  modifier_phantom_assassin_jug_death_after:OnCreated()
-        
-      
- 
-end  
+function  modifier_phantom_assassin_jug_death_after:OnCreated()
 
- 
+end  
 
 function modifier_phantom_assassin_jug_death_after:OnDestroy()
  
@@ -251,4 +135,51 @@ end
  
 function modifier_phantom_assassin_jug_death_after:GetModifierDamageOutgoing_Percentage()
     return -(self:GetStackCount() * 15)
+end
+
+modifier_phantom_assassin_death_rush_buff = class({
+    IsHidden                = function(self) return false end,
+    IsPurgable              = function(self) return false end,
+    IsDebuff                = function(self) return false end,
+    IsBuff                  = function(self) return true end,
+    RemoveOnDeath           = function(self) return true end,
+    DeclareFunctions        = function(self) return 
+        {
+            MODIFIER_PROPERTY_BASEATTACK_BONUSDAMAGE,
+            MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+            MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+            MODIFIER_PROPERTY_CASTTIME_PERCENTAGE,
+            MODIFIER_PROPERTY_BASE_ATTACK_TIME_CONSTANT,
+            MODIFIER_PROPERTY_TURN_RATE_PERCENTAGE,
+            MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
+        } end,
+    })
+
+function modifier_phantom_assassin_death_rush_buff:OnCreated()
+    self.cooldown = self:GetAbility():GetSpecialValueFor("cooldown")
+    self.attack_base = self:GetAbility():GetSpecialValueFor("attack_base")
+    self.dmg_bonus = self:GetAbility():GetSpecialValueFor("dmg_bonus")
+    self.ms_bonus = self:GetAbility():GetSpecialValueFor("ms_bonus")
+    self.as_bonus = self:GetAbility():GetSpecialValueFor("as_bonus")
+
+end
+
+function modifier_phantom_assassin_death_rush_buff:GetModifierPercentageCooldown()
+    return self.cooldown
+end
+
+function modifier_phantom_assassin_death_rush_buff:GetModifierBaseAttackTimeConstant()
+    return self.attack_base
+end
+
+function modifier_phantom_assassin_death_rush_buff:GetModifierBaseAttack_BonusDamage()
+    return self.dmg_bonus
+end
+
+function modifier_phantom_assassin_death_rush_buff:GetModifierMoveSpeedBonus_Constant()
+    return self.ms_bonus
+end
+
+function modifier_phantom_assassin_death_rush_buff:GetModifierAttackSpeedBonus_Constant()
+    return self.as_bonus
 end
---
+++ game/zombie_invasion/scripts/vscripts/items/book_of_heroes/heroes/item_npc_dota_hero_juggernaut.lua
@@ -0,0 +1,46 @@
+LinkLuaModifier("modifier_juggernaut_buff", "items/book_of_heroes/heroes/item_npc_dota_hero_juggernaut", LUA_MODIFIER_MOTION_NONE)
+item_npc_dota_hero_juggernaut = class({})
+
+function item_npc_dota_hero_juggernaut:OnSpellStart()
+		local caster = self:GetCaster()
+		local hItem = self
+
+        if not caster:HasAbility("juggernaut_buff") then 
+          caster:AddAbility("juggernaut_buff"):SetLevel(1)
+          caster:RemoveItem(hItem)
+        end
+end
+
+
+
+juggernaut_buff = class({})
+
+function juggernaut_buff:GetIntrinsicModifierName()
+    return "modifier_juggernaut_buff"
+end
+
+
+modifier_juggernaut_buff = class({
+    IsHidden                = function(self) return false end,
+    IsPurgable              = function(self) return false end,
+    IsDebuff                = function(self) return false end,
+    RemoveOnDeath           = function(self) return false end,
+})
+
+function modifier_juggernaut_buff:OnCreated()
+    self:StartIntervalThink(60)
+end
+ 
+function modifier_juggernaut_buff:OnIntervalThink()
+        self:IncrementStackCount()
+end
+
+function modifier_juggernaut_buff:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+    }
+end
+
+function modifier_juggernaut_buff:GetModifierBonusStats_Agility()
+    return self:GetStackCount() * 10
+end
