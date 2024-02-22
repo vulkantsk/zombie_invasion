7382889641f4703709a4352b005f7a9d1ab950ad
CYKAcorejznaya <119967491+CYKAcorejznaya@users.noreply.github.com>
2024-02-23 01:29:32 +0300

    F

--- content/zombie_invasion/panorama/layout/custom_game/hud/hud.js
+++ content/zombie_invasion/panorama/layout/custom_game/hud/hud.js
@@ -166,7 +166,7 @@ function SecondsToMinsNSecs(seconds) {
     return panel;
 })();
 
-const heroIconChange = ["npc_dota_hero_tusk", "npc_dota_hero_juggernaut","npc_dota_hero_huskar"]
+const heroIconChange = ["npc_dota_hero_axe_custom", "npc_dota_hero_bloodseeker_custom","npc_dota_hero_bristleback_custom","npc_dota_hero_crystal_maiden_custom","npc_dota_hero_drow_ranger_custom","npc_dota_hero_huskar_custom_custom","npc_dota_hero_juggernaut_custom","npc_dota_hero_legion_commander_custom","npc_dota_hero_medusa_custom","npc_dota_hero_nevermore_custom","npc_dota_hero_phantom_assassin_custom","npc_dota_hero_phantom_lancer_custom","npc_dota_hero_rubick_custom","npc_dota_hero_sniper_custom","npc_dota_hero_templar_assassin_custom","npc_dota_hero_tidehunter_custom","npc_dota_hero_treant_custom","npc_dota_hero_troll_warlord_custom","npc_dota_hero_tusk_custom"]
  
 const UpdateTopBar = () => {
   const cards = dotaHud.FindChildrenWithClassTraverse("TopBarPlayerSlot");
